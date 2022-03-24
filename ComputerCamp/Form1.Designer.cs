5e3dcec21a126d3db0168cf056f8e03ec6df9496
mario <mario@DESKTOP-OIDI4OL>
2022-03-24 14:49:36 -0500

    Add project files.

---
+++ ComputerCamp/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace ComputerCamp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cSharpRadioButton = new System.Windows.Forms.RadioButton();
+            this.javaRadioButton = new System.Windows.Forms.RadioButton();
+            this.mealsCheckBox = new System.Windows.Forms.CheckBox();
+            this.cabinCheckBox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.languageLabel = new System.Windows.Forms.Label();
+            this.optionsLabel = new System.Windows.Forms.Label();
+            this.eSpendingLabel = new System.Windows.Forms.Label();
+            this.totalCostLabel = new System.Windows.Forms.Label();
+            this.eSpendingTextBox = new System.Windows.Forms.TextBox();
+            this.processButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cSharpRadioButton);
+            this.groupBox1.Controls.Add(this.javaRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Language Fee";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.mealsCheckBox);
+            this.groupBox2.Controls.Add(this.cabinCheckBox);
+            this.groupBox2.Location = new System.Drawing.Point(216, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Options Fee";
+            // 
+            // cSharpRadioButton
+            // 
+            this.cSharpRadioButton.AutoSize = true;
+            this.cSharpRadioButton.Checked = true;
+            this.cSharpRadioButton.Location = new System.Drawing.Point(12, 33);
+            this.cSharpRadioButton.Name = "cSharpRadioButton";
+            this.cSharpRadioButton.Size = new System.Drawing.Size(81, 17);
+            this.cSharpRadioButton.TabIndex = 1;
+            this.cSharpRadioButton.TabStop = true;
+            this.cSharpRadioButton.Text = "C#    $1000";
+            this.cSharpRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // javaRadioButton
+            // 
+            this.javaRadioButton.AutoSize = true;
+            this.javaRadioButton.Location = new System.Drawing.Point(12, 56);
+            this.javaRadioButton.Name = "javaRadioButton";
+            this.javaRadioButton.Size = new System.Drawing.Size(81, 17);
+            this.javaRadioButton.TabIndex = 2;
+            this.javaRadioButton.TabStop = true;
+            this.javaRadioButton.Text = "Java $2000";
+            this.javaRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // mealsCheckBox
+            // 
+            this.mealsCheckBox.AutoSize = true;
+            this.mealsCheckBox.Location = new System.Drawing.Point(6, 33);
+            this.mealsCheckBox.Name = "mealsCheckBox";
+            this.mealsCheckBox.Size = new System.Drawing.Size(84, 17);
+            this.mealsCheckBox.TabIndex = 1;
+            this.mealsCheckBox.Text = "Meals  $100";
+            this.mealsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // cabinCheckBox
+            // 
+            this.cabinCheckBox.AutoSize = true;
+            this.cabinCheckBox.Location = new System.Drawing.Point(6, 56);
+            this.cabinCheckBox.Name = "cabinCheckBox";
+            this.cabinCheckBox.Size = new System.Drawing.Size(83, 17);
+            this.cabinCheckBox.TabIndex = 2;
+            this.cabinCheckBox.Text = "Cabin  $200";
+            this.cabinCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(458, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "E-Spending Money";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(124, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Language Fee";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(136, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Options Fee";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(138, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "E-Spending";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(145, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Total Cost";
+            // 
+            // languageLabel
+            // 
+            this.languageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.languageLabel.Location = new System.Drawing.Point(222, 108);
+            this.languageLabel.Name = "languageLabel";
+            this.languageLabel.Size = new System.Drawing.Size(100, 23);
+            this.languageLabel.TabIndex = 6;
+            this.languageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // optionsLabel
+            // 
+            this.optionsLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.optionsLabel.Location = new System.Drawing.Point(222, 136);
+            this.optionsLabel.Name = "optionsLabel";
+            this.optionsLabel.Size = new System.Drawing.Size(100, 23);
+            this.optionsLabel.TabIndex = 7;
+            this.optionsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // eSpendingLabel
+            // 
+            this.eSpendingLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.eSpendingLabel.Location = new System.Drawing.Point(222, 169);
+            this.eSpendingLabel.Name = "eSpendingLabel";
+            this.eSpendingLabel.Size = new System.Drawing.Size(100, 23);
+            this.eSpendingLabel.TabIndex = 8;
+            this.eSpendingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // totalCostLabel
+            // 
+            this.totalCostLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalCostLabel.Location = new System.Drawing.Point(222, 202);
+            this.totalCostLabel.Name = "totalCostLabel";
+            this.totalCostLabel.Size = new System.Drawing.Size(100, 23);
+            this.totalCostLabel.TabIndex = 9;
+            this.totalCostLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // eSpendingTextBox
+            // 
+            this.eSpendingTextBox.Location = new System.Drawing.Point(455, 40);
+            this.eSpendingTextBox.Name = "eSpendingTextBox";
+            this.eSpendingTextBox.Size = new System.Drawing.Size(100, 20);
+            this.eSpendingTextBox.TabIndex = 10;
+            // 
+            // processButton
+            // 
+            this.processButton.Location = new System.Drawing.Point(125, 248);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(75, 23);
+            this.processButton.TabIndex = 11;
+            this.processButton.Text = "Process";
+            this.processButton.UseVisualStyleBackColor = true;
+            this.processButton.Click += new System.EventHandler(this.processButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(206, 248);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 12;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(287, 248);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 13;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 297);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.processButton);
+            this.Controls.Add(this.eSpendingTextBox);
+            this.Controls.Add(this.totalCostLabel);
+            this.Controls.Add(this.eSpendingLabel);
+            this.Controls.Add(this.optionsLabel);
+            this.Controls.Add(this.languageLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Computer Camp";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton cSharpRadioButton;
+        private System.Windows.Forms.RadioButton javaRadioButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox mealsCheckBox;
+        private System.Windows.Forms.CheckBox cabinCheckBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label languageLabel;
+        private System.Windows.Forms.Label optionsLabel;
+        private System.Windows.Forms.Label eSpendingLabel;
+        private System.Windows.Forms.Label totalCostLabel;
+        private System.Windows.Forms.TextBox eSpendingTextBox;
+        private System.Windows.Forms.Button processButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ ComputerCamp/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerCamp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void processButton_Click(object sender, EventArgs e)
+        {
+            // Declare Variables
+            double languageFee = 0.0;
+            double optionsFee = 0.0;
+            double eSpending;
+            double totalCost;
+
+            // Input Data
+            // Radio Buttons
+            if(cSharpRadioButton.Checked)
+            {
+                languageFee = 1000.0;
+            }
+            else if (javaRadioButton.Checked)
+            {
+                languageFee = 2000.0;
+            }
+            // Checkboxes
+            if (mealsCheckBox.Checked)
+            {
+                optionsFee += 100.0;
+            }
+            if (cabinCheckBox.Checked)
+            {
+                optionsFee += 200.0;
+            }
+            // E-Spending Money Textbox
+            if(double.TryParse(eSpendingTextBox.Text, out eSpending))
+            {
+                if (eSpending >= 30.0 && eSpending <= 60.0)
+                {
+                    //Process Data into Info
+                    totalCost = languageFee + optionsFee + eSpending;
+
+                    //Output Information
+                    languageLabel.Text = languageFee.ToString("c");
+                    optionsLabel.Text = optionsFee.ToString("c");
+                    eSpendingLabel.Text = eSpending.ToString("c");
+                    totalCostLabel.Text = totalCost.ToString("c");
+                }
+                else
+                {
+                    MessageBox.Show("Please enter E-Spending amount between $30 and $60.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid E-Spending amount");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Radio Buttons
+            cSharpRadioButton.Checked = true;
+            // Clear Check Boxes
+            mealsCheckBox.Checked = false;
+            cabinCheckBox.Checked = false;
+            // Clear Textbox
+            eSpendingTextBox.Text = "";
+            // Clear Labels
+            languageLabel.Text = "";
+            optionsLabel.Text = "";
+            eSpendingLabel.Text = "";
+            totalCostLabel.Text = "";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
